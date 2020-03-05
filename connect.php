eba43ba28505bf909f1e84784f89089587d93ad4
Abdulrahman Mohamed <abdosoltan349@gmail.com>
2020-03-05 22:05:03 +0200

    soltan

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "sql7.freemysqlhosting.net";
-$username = "sql7325917";
-$password = "VzEp5M5Mks";
+$servername = "localhost";
+$username = "root";
+$password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=sql7325917", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=iqraa", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
